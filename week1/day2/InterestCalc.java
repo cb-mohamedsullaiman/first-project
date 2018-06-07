b238e57062eb65f5586f90474d7c1e821d829e9f
Mohamed Sullaiman <cb-mohamedsullaiman@cb-mohamedsullaiman.local>
2018-06-07 11:27:14 +0530

    InterestCalc.java -> Changes after review meeting

--- week1/day2/InterestCalc.java
+++ week1/day2/InterestCalc.java
@@ -4,24 +4,27 @@ class InterestCalc{
 	public static void main(String args[]){
 
 		Scanner scanner=new Scanner(System.in);
-		System.out.println("Press 0 for SI and 1 for CI");
-		int choice=scanner.nextInt();					//Getting the choice of user
 		System.out.println("Enter the principal amount");
 		Float principal=scanner.nextFloat();				//Getting the principal amount
 		System.out.println("Enter the number of years");
 		Float years=scanner.nextFloat();				//Getting the number of years
 		System.out.println("Ente the rate of interest");
 		Double rate=scanner.nextDouble();				//Getting the rate of interest in percentage
-		Double interest = 0.0;
+		System.out.println("Choose either 0 for SI or 1 for CI");
+		int choice=scanner.nextInt();					//Getting the choice of user (SI/CI)
 		switch(choice){
-			case 0:							//It will calculate SI
- 				interest=(principal*years*rate)/100;			//Calculating SI
+			case 0:			
+ 				Double simpInterest=(principal*years*rate)/100;			//Calculating SI
+				System.out.println("The Simple interest is "+simpInterest);
 				break;
 			case 1:						//It will calculate CI
 				System.out.println("Enter the number of times interest has to be compounded per year");		//Sometimes compound interest will be calculated many times in a year
 				int n=scanner.nextInt();
-				interest=(principal*(Math.pow((1+(rate/(n*100))),(n*years))))-principal;			//Calculating compound interest
+				Double compInterest=(principal*(Math.pow((1+(rate/(n*100))),(n*years))))-principal;			//Calculating compound interest
+				System.out.println("The Compound interest is "+compInterest);
+				break;
+			default:
+				System.out.println("Invalid input...Please choose 0 or 1");			//Throwing error if the user choice is invalid
 		}
-		System.out.println(interest);					//Printing interest
 	}
 }
