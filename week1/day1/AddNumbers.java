7e7d6c07d6ab4a1201fba52d01003b9de6e398e2
Mohamed Sullaiman <cb-mohamedsullaiman@cb-mohamedsullaiman.local>
2018-06-21 13:42:35 +0530

    Just for learning

--- week1/day1/AddNumbers.java
+++ week1/day1/AddNumbers.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class AddNumbers{
 	public static void main(String args[]){
 		Scanner scanner=new Scanner(System.in);
-		int firstNum=scanner.nextInt();			//Getting first number
+		int firstNum=scanner.nextInt();			//Getting  first number
 		int secondNum=scanner.nextInt();			//Getting second number
 		int sum=firstNum+secondNum;                  	//Adding two numbers
 		System.out.println(sum);				//Printing the number within the bracket
